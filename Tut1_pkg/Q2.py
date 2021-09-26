bd59d3c5742f85161a5eef2f35354ef95e628164
Chetan <L00163086@student.lyit.ie>
2021-09-27 00:05:41 +0530

    updated Q4

--- Tut1_pkg/Q2.py
+++ Tut1_pkg/Q2.py
@@ -6,25 +6,14 @@ bottom_icon = "v"
 left_icon = "<"
 right_icon = ">"
 
-
-print(top_icon * 48)
-print(left_icon + "\t" * 12 + right_icon)
-top_icon = "^"
-bottom_icon = "v"
-left_icon = "<"
-right_icon = ">"
-
-
-print(top_icon * 48)
-print(left_icon + "\t" * 12 + right_icon)
-print(left_icon + "\t" * 12 + right_icon)
-print(left_icon + "\t" * 12 + right_icon)
-print(left_icon + "\t" * 5 + "Title" +  "\t" * 6 + right_icon)
-print(left_icon + "\t" * 12 + right_icon)
-print(left_icon + "\t" * 12 + right_icon)
-print(left_icon + "\t" + " A \t Option a" +  "\t" * 8 + right_icon)
-print(left_icon + "\t" + " A \t Option a" +  "\t" * 8 + right_icon)
-print(left_icon + "\t" + " A \t Option a" +  "\t" * 8 + right_icon)
-print(left_icon + "\t" * 12 + right_icon)
-print(left_icon + "\t" * 12 + right_icon)
-print(bottom_icon * 48)
+print(top_icon * 30)
+print(left_icon + " " * 28 + right_icon)
+print(left_icon + " " * 28 + right_icon)
+print(left_icon + " " * 28 + right_icon)
+print(left_icon + " " * 11 + "Title"+" " * 12 + right_icon)
+print(left_icon + " " * 5 + "A Option a" + " " * 13 + right_icon)
+print(left_icon + " " * 5 + "B Option b" + " " * 13 + right_icon)
+print(left_icon + " " * 5 + "C Option c" + " " * 13 + right_icon)
+print(left_icon + " " * 28 + right_icon)
+print(left_icon + " " * 28 + right_icon)
+print(bottom_icon * 30)

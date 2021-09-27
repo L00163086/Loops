6061619a5caed9b3e0f8176e1d9a97a1aef25efd
Chetan <L00163086@student.lyit.ie>
2021-09-27 14:32:07 +0530

    loop program

---
+++ Week2_tut/TUT2/Loops.py
@@ -0,0 +1,18 @@
+"""
+# -----------------------------------------------
+# File        : Loops.py
+# Created:    : 27-09-2021 14:25
+# Author      : Chetan Raga
+# Version     : v1.0.0
+# Licencing   : (c) 2021 Chetan raga,LYIT
+               AvaiLable under GNU Public License (GPL)
+# Description :
+   
+
+#------------------------------------------------
+
+"""
+
+if __name__ == '__main':
+    grade = 50
+    if grade >= 40: print(" you passed")
---
+++ Week2_tut/TUT2/__init__.py
@@ -0,0 +1,14 @@
+"""
+# -----------------------------------------------
+# File        : __init__.py.py
+# Created:    : 27-09-2021 14:25
+# Author      : Chetan Raga
+# Version     : v1.0.0
+# Licencing   : (c) 2021 Chetan raga,LYIT
+               AvaiLable under GNU Public License (GPL)
+# Description :
+   
+
+#------------------------------------------------
+
+"""

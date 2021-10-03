a7eb308ae34287f4e1cfe3032e6fe1c578cf0c7b
Chetan <L00163086@student.lyit.ie>
2021-10-03 23:56:25 +0530

    week2 solution

---
+++ Week2_tut/TUT2/Sample_if.py
@@ -0,0 +1,17 @@
+"""
+# -----------------------------------------------
+# File        : Sample_if.py
+# Created:    : 27-09-2021 14:37
+# Author      : Chetan Raga
+# Version     : v1.0.0
+# Licencing   : (c) 2021 Chetan raga,LYIT
+               AvaiLable under GNU Public License (GPL)
+# Description :
+   
+
+#------------------------------------------------
+
+"""
+if __name__ == '__main__':
+    grade = 50
+    if grade >+ 40 : print("you passed!")
